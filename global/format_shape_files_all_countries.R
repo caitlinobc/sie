b570bd644d640fc36ac94cadd5a5d290a409c39b
Caitlin O'Brien-Carelli <caitlinobc@gmail.com>
2020-12-03 13:54:46 -0500

    fix topology error in drc shape file

--- global/format_shape_files_all_countries.R
+++ global/format_shape_files_all_countries.R
@@ -105,6 +105,7 @@ i = i+1
 
 if (drc_patch==TRUE) {
   
+  #--------------------
   # import the health zone level shape file
   drc = shapefile(paste0(dir, 'drc_zones/RDC_Zones de santé.shp'))
   drc_names = data.table(cbind(district = drc@data$Nom,
@@ -112,21 +113,30 @@ if (drc_patch==TRUE) {
   drc_names[ , country:='DRC']
 
   # create health zone labels
-  labels_drc = data.table(coordinates(drc))
-  setnames(labels_drc, c('long', 'lat'))
-  labels_drc = cbind(labels_drc,drc_names)
+  drc_labels = data.table(coordinates(drc))
+  setnames(drc_labels, c('long', 'lat'))
+  drc_labels = cbind(drc_labels,drc_names)
   
-  # --------------------
+  # correct the topology intersection in the shape file
+  drc2 = gBuffer(drc, byid=TRUE, width = 0)
+  drc2$id = drc$Pcode
+  
   # fortify the shape file
-  coord = data.table(fortify(shape, region = region_code)) 
-  coord[ , country:=country] # label the country of the shape file 
+  drc_coord = data.table(fortify(drc2, region='Pcode'))
+  drc_coord[ , country:='COD'] 
+  #--------------------
   
-
+  #--------------------
+  # remove DRC from the main file and replace with health zone level
+  full_shape = full_shape[country!='COD']
+  full_shape = rbind(full_shape, drc_coord)
+  
+  full_names = full_names[country!='COD']
+  full_names = rbind(full_names, drc_names)
+  
+  full_labels = full_labels[country!='COD']
+  full_labels = rbind(full_labels, drc_labels)
 }
-
-
-
-sort(drc@data$Nom)
 
 
 # --------------------
