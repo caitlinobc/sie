cf6cfc5851bb8a0894740dfaeee9a9d9010ee939
Caitlin O'Brien-Carelli <caitlinobc@gmail.com>
2021-01-05 17:50:46 -0500

    Create maps

--- global/ahd/ahd_first_map_set.R
+++ global/ahd/ahd_first_map_set.R
@@ -20,6 +20,7 @@ library(rgeos)
 library(maptools)
 library(broom)
 library(ggrepel)
+library(grid)
 library(gridExtra)
 library(readxl)
 # --------------------
@@ -56,16 +57,64 @@ ggplot(shape[country=='Malawi'], aes(x=long, y=lat, group=group, fill=1)) +
   coord_fixed()+                         
   geom_polygon()+ 
   geom_path(size=0.01)+
-  scale_fill_gradientn(colors = '#f7fbff')+
+  scale_fill_gradientn(colors = '#c7e9c0')+
   geom_point(data = dt[country=='Malawi'], aes(x = long, y =lat, group = country), 
-             size = 2, colour = '#feb24c', alpha= 0.5)+
+             size = 3, colour = '#feb24c', alpha= 0.5)+
   theme_void(base_size = 14) +
-  labs(title="Health facilities included in the AHD Study",
-       subtitle = 'Malawi') +
+  labs(title="AHD study facilities: Malawi",
+       subtitle = expression(italic("n = 21"))) +
+  theme(plot.title=element_text(vjust=-1),
+        text=element_text(size=16),
+        legend.position = 'none') 
+
+# -------------------
+# simple introductory map of tanzania
+
+ggplot(shape[country=='Tanzania'], aes(x=long, y=lat, group=group, fill=1)) + 
+  coord_fixed()+                         
+  geom_polygon()+ 
+  geom_path(size=0.01)+
+  scale_fill_gradientn(colors = '#7fcdbb')+
+  geom_point(data = dt[country=='Tanzania'], aes(x = long, y =lat, group = country), 
+             size = 4, colour = '#253494', alpha= 0.5)+
+  theme_void(base_size = 14) +
+  labs(title="AHD study facilities: Tanzania",
+       subtitle = expression(italic("n = 9"))) +
   theme(plot.title=element_text(vjust=-1), 
         plot.caption=element_text(vjust=6),
-        text=element_text(size=16)) 
+        text=element_text(size=16),
+        legend.position = 'none') 
+
+# -------------------
+# study facilities side by side 
 
 
+# malawi
+f1 = ggplot(shape[country=='Malawi'], aes(x=long, y=lat, group=group, fill=1)) + 
+  coord_fixed()+                         
+  geom_polygon()+ 
+  geom_path(size=0.01)+
+  scale_fill_gradientn(colors = '#c7e9c0')+
+  geom_point(data = dt[country=='Malawi'], aes(x = long, y =lat, group = country), 
+             size = 3, colour = '#feb24c', alpha= 0.5)+
+  theme_void() +
+  labs(title="Malawi") +
+  theme(plot.title=element_text(hjust=0.2),
+    legend.position = 'none') 
 
+f2 = ggplot(shape[country=='Tanzania'], aes(x=long, y=lat, group=group, fill=1)) + 
+  coord_fixed()+                         
+  geom_polygon()+ 
+  geom_path(size=0.01)+
+  scale_fill_gradientn(colors = '#c7e9c0')+
+  geom_point(data = dt[country=='Tanzania'], aes(x = long, y =lat, group = country), 
+             size = 3, colour = '#feb24c', alpha= 0.5)+
+  theme_void() +
+  labs(title="Tanzania") +
+  theme(plot.title=element_text(hjust=0.2),
+        legend.position = 'none') 
+
+# plot the maps side by side
+grid.arrange(f1, f2, nrow=1, top = textGrob("AHD Study Health Facilities",
+        gp=gpar(fontsize=18,font=2)))
 
