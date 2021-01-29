2541d61734d5d287bd7926e8b1bbc4e81e076100
caitlinobc <caitlinobc@gmail.com>
2021-01-29 14:37:26 -0500

    Write an amazing lapply and win life

--- global/ahd/ahd_data_prep.R
+++ global/ahd/ahd_data_prep.R
@@ -3,7 +3,7 @@
 #
 # 1/28/21
 # Prep data from the AHD study for visualization
-# Check fordata erros 
+# Check for errors or quality concerns in the data 
 # ----------------------------------------------
 
 # --------------------
@@ -16,6 +16,7 @@ library(tidyr)
 library(ggplot2)
 library(RColorBrewer)
 library(readxl)
+library(lubridate)
 # --------------------
 # Files and directories
 
@@ -42,6 +43,17 @@ dt[ ,c(drop_names, 'dov', 'gxtest_dt', 'gxpos'):=NULL]
 
 # --------------------
 # convert dates to date types
+
+# all date names include dt except dob
+dates = c(names(dt)[ grepl("dt", names(dt))], 'dob')
+
+date_byVars = names(dt)[!(names(dt) %in% dates)]
+dt = dt[ , lapply(.SD, as.Date), by = date_byVars]
+
+
+
+
+# convert dates to date format
 dt[ , ahd_dt:=as.Date(ahd_dt)]
 dt[ , dob:=as.Date(dob)]
 dt[ , dtpos:=as.Date(dtpos)]
@@ -50,17 +62,59 @@ dt[ , whostage1st_dt:=as.Date(whostage1st_dt)]
 
 dt[ , tptstart_dt:=as.Date(tptstart_dt)]
 dt[ , tptalready_dt:=as.Date(tptalready_dt)]
-dt[ , sstest_dt:=as.Date(sstest_dt)]
-dt[ , hvl6m_dt:=as.Date(hvl6m_dt)]
-dt[ , tbtxcplt_dt:=as.Date(tbtxcplt_dt)]
+dt[ , tptcplt_dt:=as.Date(tptcplt_dt)]
 
+dt[ , sstest_dt:=as.Date(sstest_dt)]
+dt[ , firstart_dt:=as.Date(firstart_dt)]
+dt[ , hvl6m_dt:=as.Date(hvl6m_dt)]
+
+dt[ , tbtxstart_dt:=as.Date(tbtxstart_dt)]
+dt[ , tbtxalready_dt:=as.Date(tbtxalready_dt)]
+dt[ , tptxcplt_dt:=as.Date(tptxcplt_dt)]
+
+dt[ , firstvis:=as.Date(firstvis)]
+dt[ , cd4_fvis_dt:=as.Date(cd4_fvis_dt)]
+
+dt[ , ahd_u5_dt:=as.Date(ahd_u5_dt)]
+dt[ , ahd_new_cd4_dt:=as.Date(ahd_new_cd4_dt)]
+dt[ , ahd_new_who_dt:=as.Date(ahd_new_who_dt)]
+dt[ , ahd_oclin_who_dt:=as.Date(ahd_oclin_who_dt)]
+
+dt[ , ahd_tb_dt:=as.Date(ahd_tb_dt)]
+dt[ , ahd_incwho_dt:=as.Date(ahd_incwho_dt)]
+dt[ , ahd_hvl_dt:=as.Date(ahd_hvl_dt)]
+dt[ , ahd_cd4u200_dt:=as.Date(ahd_cd4u200_dt)]
+
+
+# the following variable appears as POSIX CT but is an import error
+setnames(dt, 'ahd_whostage_incwho', 'ahd_whostage_incwho1' )
+dt[grepl(3, ahd_whostage_incwho1), ahd_whostage_incwho:=3]
+dt[grepl(4, ahd_whostage_incwho1), ahd_whostage_incwho:=4]
+dt[ , ahd_whostage_incwho1:=NULL] #after this, should be no more POSIXCT in data
 
 # --------------------
 # convert1/0s to logicals
 
+byVars = c(dates, 'siteid')
+
+
+
+
+dt[ , knwstat:=as.logical(knwstat)]
+
+
+
+
+# variables with only one value...
+c('knwstat')
+
 
 # --------------------
-# use factor labelling on a few variables
+# convert eligibility classification to a factor
+
+dt$ahd_elig = as.factor(dt$ahd_elig)
+
+
 
 
 
