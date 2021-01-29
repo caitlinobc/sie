556312ed032a74bfdee25052b8bb9c3c58818522
caitlinobc <caitlinobc@gmail.com>
2021-01-29 18:05:49 -0500

    Update plots

--- global/ahd/ahd_data_prep.R
+++ global/ahd/ahd_data_prep.R
@@ -25,7 +25,7 @@ library(eeptools)
 dir = 'C:/Users/ccarelli/OneDrive - E Glaser Ped AIDS Fdtn/AHD/'
 
 # set working directory to source other scripts
-setwd('C:/Users/ccarelli/Documents/GitHub/sie/all/ahd/')
+setwd('C:/Users/ccarelli/Documents/GitHub/sie/global/ahd/')
 
 # --------------------
 # source additional code files 
@@ -34,7 +34,7 @@ setwd('C:/Users/ccarelli/Documents/GitHub/sie/all/ahd/')
 run_plots = TRUE
 
 # run data quality checks? 
-dq_checks = TRUE
+dq_checks = FALSE
 #---------------------------------------
 # TANZANIA BASELINE DATA PREP
 
@@ -70,15 +70,26 @@ dt[grepl(4, ahd_whostage_incwho1), ahd_whostage_incwho:=4]
 dt[ , ahd_whostage_incwho1:=NULL] #after this, should be no more POSIXCT in data
 
 # --------------------
+# calculate age 
+
+# age represents age at enrollment in the study
+# treat like an age - all ages rounded down to the closest year
+dt[!is.na(dob), age:=floor((age_calc(dob, enddate = ahd_dt, 
+                                     units = 'years', precise = TRUE)))]
+
+# --------------------
+
+# --------------------
 # convert1/0s to logicals
 
-idVars = c('pid', 'siteid', 'dhisid', 'dhisname', 'region', 'district')
+idVars = c('pid', 'siteid', 'dhisid', 'dhisname', 'region', 'district', 'age')
 
 # create a list of variables that should not be converted to logicals
 log_byVars = c(idVars, dates, 'ahd_elig', 'cd4_after_ahdelig_result', 
             'whostage1st', 'hvl6mcat', 'hvl6mresult', 'whostage_fvis', 
             'transferinid', 'cd4_fvis', 'ahd_new_cd4result', 'ahd_new_whostage', 
-            'ahd_oclin_whostage', 'ahd_hvlresult', 'ahd_hvlcat', 'ahd_cd4result')
+            'ahd_oclin_whostage', 'ahd_hvlresult', 'ahd_hvlcat', 'ahd_cd4result',
+            'ahd_whostage_incwho')
 
 #convert remaining variables to logicals
 dt = dt[ , lapply(.SD, as.logical), by = log_byVars]
@@ -89,53 +100,35 @@ dt = dt[ , lapply(.SD, as.logical), by = log_byVars]
 dt$ahd_elig = factor(dt$ahd_elig, levels = c(1:6), labels = c('Newly diagnosed',
       'LTFU and returned', 'On ART vir failure', 'On ART AIDS ill',
       'Under 5', 'CD4 < 200'))
-# --------------------
-# calculate age 
-
-# age represents age at enrollment in the study
-# treat like an age - all ages rounded down to the closest year
-dt[!is.na(dob), age:=floor((age_calc(dob, enddate = ahd_dt, 
-             units = 'years', precise = TRUE)))]
 
 # if a patient is under 5, their eligibility reason should be under 5
 # priority ranking lists under 5 as top (if more than one eligibility)
 dt[age < 5, ahd_elig:='Under 5']
 
 # --------------------
+# convert side id to character to avoid confusion during analysis
+
+dt[ , siteid:=as.character(siteid)]
+
+# --------------------
 # add categories to subset while doing analysis
+ 
+
+#-------------------------------------------------
+# SOURCE OUTSIDE CHECKS ON DATA 
+
+# run diagnostic plots to examine variables
+if (run_plots==T)  source('ahd_disgnostic_plots.R')
+
+# run data quality checks and export csv examples of issues
+if (dq_checks==T)  source('ahd_dq_checks.R')
+
+#-------------------------------------------------
+# SAVE THE PREPPED FILE 
 
 
 #-------------------------------------------------
 
 
-if (run_plots==T) source('/ahd_diagnostic_plots.R')
-#-------------------------------------------------
-# DATA QUALITY
-
-# --------------------
-# examine the data quality challenges
-
-# export a list of sites to compare to study criterion
-sites = dt[ ,.(siteid = unique(siteid)), by = .(dhisid, dhisname, region, district)]
-sites = sites[!is.na(dhisid)]
-sites = dt[ ,.(siteid, dhisid, dhisname, region, district)] # put siteid first
-write.csv(sites, paste0(dir, '/data/prepped/sites_included.csv'))
-
-# patient ids: these counts should be the same (length of sheet and length of data)
-dt[ ,length(unique(pid))]
-nrow(dt)
-
-# export a list of repeat patient identifiers
-dt[ , pid_count:=.N, by = pid]
-pids = dt[1 < pid_count]
-pids = pids[order(pid)]
-write.csv(pids, paste0(dir, '/data/prepped/repeat_pt_ids.csv'))
-
-# export the rows with substantial missing data 
-missing_rows = dt[is.na(ahd_dt)]
-write.csv(missing_rows, paste0(dir, '/data/prepped/rows_with_missing_data.csv'))
-
-# --------------------
 
 
-#WHY ISNT THIS CHANGING
--- global/ahd/ahd_disgnostic_plots.R
+++ global/ahd/ahd_disgnostic_plots.R
@@ -1,14 +1,130 @@
-# diagnostic plots
+#-------------------------
+# Diagnostic Plots
+# Sourced from ahd_data_prep.R
+#-------------------------
+
+#-------------------------
+# color palettes 
+tri_colors = c('#b2182b', '#fdae61', '#74add1')
+bar_colors = c('#d73027', '#f46d43', '#fdae61', '#74add1', '#4575b4')
+
+#-------------------------
+# age classifications
 
 # histogram of age
-hist(dt$age)
+h1 = hist(dt$age)
+
+hist(dt$cd4_after_ahdelig_result)
 
 # jittered scatter of age by enrollment classification
-ggplot(dt, aes(x = ahd_elig, y = age, color = ahd_elig))+
+g1 = ggplot(dt, aes(x = ahd_elig, y = age, color = ahd_elig))+
          geom_jitter()+
          theme_bw()
 
 # box plot of age by enrollment classification
-ggplot(dt, aes(x = ahd_elig, y = age))+
+g2 = ggplot(dt, aes(x = ahd_elig, y = age))+
   geom_boxplot()+
   theme_bw()
+
+# scatter of age by CD4 at enrollment
+ggplot(dt, aes(x = cd4_after_ahdelig_result, y = age))+
+  geom_point()+
+  theme_bw()
+
+# scatter of age by CD4 at enrollment with eligibility classification
+ggplot(dt, aes(x = cd4_after_ahdelig_result, y = age, color = ahd_elig))+
+  geom_point()+
+  theme_bw()
+
+#-------------------------
+
+#-------------------------
+# plot the logical variables by site just for counts
+
+# create a data set solely composed of logicals
+log_vars = names(dt)[!(names(dt) %in% log_byVars)]
+dt_long = melt(dt, id.vars = idVars , measure.vars = log_vars)
+
+# loop through the variables and plot them
+vars = dt_long[,unique(variable)]
+
+# createa new data set for the logicals reflecting response counts
+i = 1
+for (v in vars){
+var_name = v
+
+# create a data set ofcounts of responses for this variable by site
+trues = dt_long[variable==v, .(value = sum(value, na.rm=T), category = 'Yes'), by=siteid]
+
+falses = dt_long[variable==v & value==FALSE, .(value = length(unique(pid)), 
+                            category = 'No'), by=siteid]
+
+missing = dt_long[variable==v & is.na(value), .(value = length(unique(pid)), 
+                                               category = 'Missing'), by=siteid]
+dt_new = rbind(trues, falses, missing)
+dt_new[ ,variable:=v]
+
+# bind it to the other variables
+if (i==1) dt_counts = dt_new
+if (1 < i) dt_counts = rbind(dt_counts, dt_new)
+
+i = i+1}
+
+# factor the counts to plot bars in order
+dt_counts$category = factor(dt_counts$category, levels = c('Yes', 'No', 'Missing'), 
+                     labels = c('Yes', 'No', 'Missing'))
+
+#-------------------------
+# counts of eligibility by site
+
+
+counts = dt[, .(value=length(unique(pid))), by =.(ahd_elig, siteid)]
+
+
+ggplot(counts, aes(x=siteid, y=value, fill=ahd_elig)) +
+  geom_bar(position = 'dodge', stat = 'identity') +
+  geom_text(position = position_dodge(width=0.9), aes(label=value), vjust=-0.5)+
+  scale_fill_manual(values = bar_colors, na.value = '#bababa')+
+  theme_bw()+
+  labs(x = 'Site ID', y = 'Count of responses',
+       title = "AHD Study eligibility criterion, count of patients enrolled by site",
+       subtitle = 'Tanzania Baseline Cohort',
+       fill ='Reason for enrollment')
+
+
+#-------------------------
+
+test_plots = NULL
+
+i = 1
+for (v in vars) {
+
+plot_title = v
+
+test_plots[[i]] = ggplot(dt_counts[variable==v], aes(x=siteid, y=value, fill=category)) +
+  geom_bar(position = 'dodge', stat = 'identity') +
+  geom_text(position = position_dodge(width=0.9), aes(label=value), vjust=-0.5)+
+  scale_fill_manual(values = c('#b2182b', '#fdae61', '#74add1')) +
+  theme_bw()+
+  labs(x = 'Site ID', y = 'Count of responses',
+        title = paste0('Variable name: ', plot_title), 
+       fill ='Response')
+
+i =i+1
+}
+
+
+#-------------------------
+
+pdf(width=12, height = 9, paste0(dir, 'outputs/diagnostic_plots.pdf'))
+
+h1 
+g1
+g2
+test_plots
+
+dev.off()
+
+
+
+
