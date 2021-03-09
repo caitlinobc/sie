644313b0a8f6656aa77cee6d25cc7920fc749253
caitlinobc <caitlinobc@gmail.com>
2021-03-09 16:53:33 -0500

    Update esw_test_map.R

--- all/pbi_maps_code/esw_test_map.R
+++ all/pbi_maps_code/esw_test_map.R
@@ -20,7 +20,7 @@ library(maptools)
 library(broom)
 library(ggrepel)
 library(gridExtra)
-library(geojsonio)
+
 # --------------------
 # Files and directories
 
@@ -31,7 +31,7 @@ dir = 'C:/Users/ccarelli/OneDrive - E Glaser Ped AIDS Fdtn/data/pbi_maps/'
 OutDir = paste0(dir, 'outputs/' )
 
 # set the working directory for the shape file
-setwd(paste0(dir, '/district_rds/'))
+setwd(paste0(dir, '/rds_shape_files/'))
 
 # --------------------
 # load the function that fixes diacritical marks
@@ -40,26 +40,47 @@ source('C:/Users/ccarelli/Documents/GitHub/sie/all/fix_diacritics_function.r')
 # --------------------
 # import the data 
 
-reg_dt = data.table(read.csv(paste0(dir, 'pepfar_test_data/ew_test_data.csv')))
+dt = data.table(read.csv(paste0(dir, 'pepfar_test_data/esw_test_data_district.csv')))
 
 # --------------------
 # import the shape file, check, and fortify
 
 # import and plot the shape file
-shape = readRDS('gadm36_CMR_3_sp.rds') # district level
+shape = readRDS('gadm36_SWZ_2_sp.rds') # district level
 plot(shape)
 
 # list the specific districts
-shp = data.table(fortify(shape, region = 'GID_3'))
+shp = data.table(fortify(shape, region = 'GID_2'))
 
 # --------------------
 # determine the shape names for the merge
 
 # create a list of names
-names = data.table((cbind(district = shape@data$NAME_3, id = shape@data$GID_3)))
+names = data.table((cbind(district = shape@data$NAME_2, id = shape@data$GID_2)))
 shp = merge(shp, names, by = 'id', all.x = TRUE)
 
 # fix diacritical marks in the shape file 
-shp[ , district:=fix_diacritics(district)]
+# shp[ , district:=fix_diacritics(district)]
 
 # --------------------
+# check if the names match
+
+# is each region included in the data 
+dt[ ,unique(district)] %in% shp[,unique(district)]   
+
+# print the names that are in the data but not the shape file
+shape_names = shp[,unique(district)]   
+dt[!(district %in% shape_names), unique(district)]
+
+# --------------------
+
+View(shp[ ,.(district = unique(district))])
+
+
+shp[grepl('Shisel',district), unique(district)]
+
+
+
+
+
+
