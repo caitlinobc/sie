7ec7d9bb8ffcaf0e2750bda557984f93aea25eb3
caitlinobc <caitlinobc@gmail.com>
2021-03-11 15:19:06 -0500

    Test map for MESO meeting

--- all/pbi_maps_code/test_map.R
+++ all/pbi_maps_code/test_map.R
@@ -1,8 +1,9 @@
 # ----------------------------------------------
 # Caitlin O'Brien-Carelli
 #
-# Create a working Eswaitni map for use in PBI
-# Create Admin 1 and 2 maps 
+# Function to diagnose areas that do not match
+# Compared EGPAF admin levels to shape files
+# creates a list of "unmatching" administrative areas
 # ----------------------------------------------
 
 # --------------------
@@ -25,7 +26,7 @@ library(gridExtra)
 # Files and directories
 
 # set the working directory to the cameroon data
-dir = 'C:/Users/ccarelli/OneDrive - E Glaser Ped AIDS Fdtn/data/pbi_maps/'
+dir = 'C:/Users/ccarelli/OneDrive - E Glaser Ped AIDS Fdtn/data/pbi_maps/sample_map/'
 
 # set the output directory
 OutDir = paste0(dir, 'outputs/' )
@@ -34,67 +35,74 @@ OutDir = paste0(dir, 'outputs/' )
 pep_dir = 'C:/Users/ccarelli/OneDrive - E Glaser Ped AIDS Fdtn/data/pepfar_org_units/'
 
 # set the working directory for the shape file
-setwd(paste0(dir, '/rds_shape_files/'))
+setwd(dir)
 
 # --------------------
-# load the function that fixes diacritical marks
-source('C:/Users/ccarelli/Documents/GitHub/sie/all/fix_diacritics_function.r')
-
-# --------------------
-
-# ----------------------------------------------
 # import the data 
-
-dt = data.table(read.csv(paste0(dir, 'pepfar_test_data/esw_test_data_district.csv')))
+dt = data.table(read.csv(paste0(dir, 'esw_test_data_district.csv')))
 
 # import the list of pepfar facilities
-pp = data.table(readRDS(paste0(pep_dir, 'prepped/datim_health_facilities.rds')))
+pp = data.table(readRDS(paste0(pep_dir, 
+            'prepped/datim_health_facilities.rds')))
 
 # --------------------
 # import the shape file, check, and fortify
 
 # import and plot the shape file
-shape = readRDS('gadm36_SWZ_2_sp.rds') # district level
+shape = readRDS(paste0(dir, 'gadm36_ESW_2_sp.rds')) 
 plot(shape)
 
 # list the specific districts
-shp = data.table(fortify(shape, region = 'GID_2'))
+shp = fortify(shape, region = paste0('GID_', level))
 
 # --------------------
 # determine the shape names for the merge
 
-# create a list of names
-names = data.table((cbind(district = shape@data$NAME_2, id = shape@data$GID_2)))
+# create a list of names for the districts to add to the shape file
+# if statement based on shape file administrative level
+if (level==1) {names = data.table(cbind(district = shape@data$NAME_1,
+    id = shape@data$GID_1))} else if (level==2) {
+      names = data.table(cbind(district = shape@data$NAME_2,
+      id = shape@data$GID_2))} else {
+        names = data.table(cbind(district = shape@data$NAME_3,
+       id = shape@data$GID_3))}
+
+# if mapping at the regional level, use region for the column name
+setnames(names, 'district', admin ) 
+
+# merge in the names
 shp = merge(shp, names, by = 'id', all.x = TRUE)
 
-# fix diacritical marks in the shape file 
-# shp[ , district:=fix_diacritics(district)]
-
-# --------------------
+# ----------------------------------------------
 # check if the names match
 
+
+# REGIONAL CHECK
 # is each region included in the data 
-dt[ ,unique(district)] %in% shp[,unique(district)]   
+dt[ ,unique(region)] %in% shp[,unique(region)] 
 
 # print the names that are in the data but not the shape file
-shape_names = shp[,unique(district)]   
-dt[!(district %in% shape_names), unique(district)]
+shape_names = shp[,unique(region)]   
+dt[!(region %in% shape_names), unique(region)]
 
+
+# # is each region included in the data 
+# dt[ ,unique(district)] %in% shp[,unique(district)]   
+# 
+# 
+# # print the names that are in the data but not the shape file
+# shape_names = shp[,unique(district)]   
+# dt[!(district %in% shape_names), unique(district)]
 # --------------------
 
 # ----------------------------------------------
 # map the pepfar org units
 
-pp = pp[country=='ESW' & egpaf==TRUE]
+pp = pp[country=='CDI' & egpaf==TRUE]
 
 
 pp[ , lat:=as.numeric(lat)]
 pp[ , long:=as.numeric(long)]
-
-egpaf_dists = dt[,unique(district)]
-
-
-alt = data.table(country = 'ESW', lat = -27.1211, long = 31.2079)
 
 
 ggplot(shp, aes(x=long, y=lat, group=group, fill=1)) + 
@@ -102,12 +110,9 @@ ggplot(shp, aes(x=long, y=lat, group=group, fill=1)) +
   geom_polygon()+ 
   geom_path(size=0.01)+
   scale_fill_gradientn(colors = '#fff7fb')+
-  geom_point(data = pp[grepl('Shis', district)], aes(x = long, y =lat, color = district, group = factor(country)),
+  geom_point(data = pp, aes(x = long, y =lat, color = region, group = factor(country)),
               inherit.aes = FALSE,
              size = 2, alpha = 0.5)+
-  geom_point(data = alt, aes(x = long, y =lat, group = factor(country)),
-             inherit.aes = FALSE,
-             size = 6, alpha = 0.5)+
   theme_void(base_size = 14) 
 
 
@@ -130,6 +135,8 @@ colour = '#253494'
 # ----------------------------------------------
 # code to change the shape file
   
+  # fix diacritical marks in the shape file 
+  # shp[ , district:=fix_diacritics(district)]
   
   
 # ESWATINI
