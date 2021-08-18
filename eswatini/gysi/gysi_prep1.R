d63b56f179cd19750134fc217063a106cf674d9f
caitlinobc <caitlinobc@gmail.com>
2021-08-18 13:31:20 -0700

    Update gysi_prep1.R

--- eswatini/gysi/gysi_prep1.R
+++ eswatini/gysi/gysi_prep1.R
@@ -79,10 +79,35 @@ dt$age = factor(dt$age,c("<1", "1-4", "5-9",  "10-14", "15-19",
                      "45-49", "50+")) 
 
 # --------------------
-# create detailed age categories
+# create summary age categories
 
+# Child, youth, adult category
+dt[age %in% c("<1", "1-4", "5-9", "10-14"), cya:='Child']
+dt[age %in% c("15-19","20-24"), cya:='Youth']
+dt[age %in% c("25-29", "30-34", "35-39", "40-44",
+              "45-49", "50+"), cya:='Adult']
+
+# child, adolescent, young adult, adult
+dt[age %in% c("<1", "1-4", "5-9"), caya:='Child']
+dt[age %in% c("10-14", "15-19"), caya:='Adolescent']
+dt[age %in% c("20-24"), caya:='Youth']
+dt[age %in% c("25-29", "30-34", "35-39", "40-44",
+              "45-49", "50+"), caya:='Adult']
+
+# factor the results
+dt$cya = factor(dt$cya, c('Child', 'Youth', 'Adult'),
+                c('Child', 'Youth', 'Adult'))
+
+dt$caya = factor(dt$caya, c('Child', 'Adolescent', 'Youth', 'Adult'),
+                c('Child', 'Adolescent', 'Youth', 'Adult'))
 
 # --------------------
+# sum to the national level
+
+# national - all variables - subset to just fy20
+dt_all = dt[grepl('20', fq), .(value = sum(value, na.rm = T)),
+                      by = .(sex, age, cya, caya, variable)]
+
 
 
 # ----------------------------------------------
@@ -95,5 +120,31 @@ dt$age = factor(dt$age,c("<1", "1-4", "5-9",  "10-14", "15-19",
 
 
 
+# --------------------
+# Descriptive statistics
+
+# HIV testing disparities
+test_tot = dt_all[variable=='hts_tst', sum(value)]
+dt_all[variable=='hts_tst' & sex=='Male', sum(value)/test_tot]
+dt_all[variable=='hts_tst' & sex=='Female', sum(value)/test_tot]
+
+
+
 # ----------------------------------------------
+
+pdf(paste0(dir, 'writeup_figures.pdf'), height = 9, width = 16 )
+
+ggplot(dt_all[variable=='hts_tst'], 
+       aes(x=age, y=value, fill = sex)) +
+  geom_bar(position = 'dodge', stat = 'identity') +
+  geom_text(position = position_dodge(width=0.9), 
+            aes(label=value), vjust=-0.5)+
+  scale_fill_manual(values = c('#b2182b', '#fdae61')) +
+  theme_bw()+
+  labs(x = 'Age Category', y = 'Tested for HIV',
+       # title = ('Clients tested for HIV by sex and age category, FY20'), 
+       fill ='Sex') +
+  theme(text = element_text(size=20)) 
+
+dev.off()
 
