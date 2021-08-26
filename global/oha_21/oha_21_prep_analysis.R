be29cef66c7d6aee7a9594819ae7950268c5edd2
Caitlin Carelli <caitlinobc@gmail.com>
2021-08-26 11:08:21 -0700

    Update oha_21_prep_analysis.R

--- global/oha_21/oha_21_prep_analysis.R
+++ global/oha_21/oha_21_prep_analysis.R
@@ -37,16 +37,72 @@ mapDir = 'C:/Users/ccarelli/OneDrive - E Glaser Ped AIDS Fdtn/data/maps/r_shape_
 # read in the data 
 dt = data.table(read.csv(paste0(dir, 'oha_21_prep.csv')))
 
-
 # ----------------------------------------------
 
 # --------------------
 # prep the data for analysis
 
 # rename the variables for analysis 
-setnames(dt, c('country', 'sex', 'age', 'prep_curr', 'prep_new', 'fq'))
-
+setnames(dt, c('award','country', 'prep_curr', 'prep_new',
+               'fq', 'date', 'sex', 'age'))
+               
 # shape the data long for visualization
-dt = melt(dt, id.vars = c('country', 'sex', 'age', 'fq'))
+idVars = c('award','country', 'sex', 'age', 'fq', 'date')
+dt = melt(dt, id.vars = idVars)
 
 # --------------------
+# factor the variables
+
+dt$fq = factor(dt$fq, c("FY20 Q2", "FY20 Q4",
+        "FY21 Q1", "FY21 Q2", "FY21 Q3"),
+        c("Q2 FY20", "Q4 FY20",
+          "Q1 FY21", "Q2 FY21", "Q3 FY21"))
+
+dt$date = factor(dt$date, c('Jan-Mar 2020', 'Oct-Dec 2020',
+      'Jul-Sep 2020', 'Jan-Mar 2021', 'Apr-Jun 2021'), 
+      c('Jan-Mar 2020', 'Oct-Dec 2020',
+        'Jul-Sep 2020', 'Jan-Mar 2021', 'Apr-Jun 2021'))
+
+dt$age = factor(dt$age, c("Unknown Age", "15-19", "20-24", "25-29",
+                          "30-34", "35-39", "40-44", "45-49", "50+"),
+                c("Unknown", "15-19", "20-24", "25-29",
+                  "30-34", "35-39", "40-44", "45-49", "50+")) 
+
+# format the grant names to match style guide
+dt[award=='USAID RHITES SW', award:='USAID RHITES-SW']
+dt[award=='USAID Eswatini', award:='USAID EHPCS']
+
+# create an award name variable without the donor name
+dt[ , award_alt:=trimws(gsub('USAID', '', award))]
+
+# rearrange the order
+dt = dt[ ,.(value), by = .(country, award, award_alt, fq, date,
+        sex, age, variable)]
+
+# --------------------
+
+# ----------------------------------------------
+# export an Excel file for Excel graphs 
+dt_excel = copy(dt)
+
+# change the names of variables to appear correctly on graphs
+dt_excel[variable=='prep_curr', variable:='PREP_CURR']
+dt_excel[variable=='prep_new', variable:='PREP_NEW']
+setnames(dt_excel, c('Country', 'Award Full', 'Award', 'Quarter',
+                     'Fiscal Quarter', 'Sex', 'Age', 'Variable',
+                     'Value'))
+
+# export the data, over writing the previous version 
+write.xlsx(dt_excel, paste0(outDir, 'OHA_21_PREP_figures.xlsx'), overwrite = T)
+
+# delete the data set
+dt_excel = NULL
+
+# ----------------------------------------------
+
+# --------------------
+# do some mapping
+
+
+
+
