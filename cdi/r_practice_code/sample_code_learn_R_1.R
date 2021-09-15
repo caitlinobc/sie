1e3a7cb2bac9853b3c8cf47976fd06fc73d07c62
Caitlin Carelli <caitlinobc@gmail.com>
2021-09-15 13:24:49 -0700

    Update sample_code_learn_R_1.R

--- cdi/r_practice_code/sample_code_learn_R_1.R
+++ cdi/r_practice_code/sample_code_learn_R_1.R
@@ -1,7 +1,7 @@
 # --------------------------------------------
 # Caitlin O'Brien-Carelli
 #
-# 4/29/21
+# 9/15/21
 # Sample code for the Dream Team
 # --------------------------------------------
 
@@ -14,6 +14,7 @@ rm(list=ls()) # clears the workspace
 # install important R packages on your computer
 # you only need to run this section one time 
 # once the packages are installed, you can add # in front of the installation code
+# then they won't run again
 
 install.packages('Rtools') # this package helps other packages
 install.packages('readxl')
@@ -23,6 +24,7 @@ install.packages('RColorBrewer')
 
 # --------------------
 # load the packages you need to run this code file
+# each time you use R, you need to load the packages you need
 
 library(readxl) # loads excel files
 library(data.table) # manipulates data
@@ -40,10 +42,12 @@ dir = 'C:/Users/ccarelli/OneDrive - E Glaser Ped AIDS Fdtn/CDI/data/r_training_d
 
 # --------------------
 # load the data 
+# the paste0 function pastes the location of the file (dir) to the name of the file
+# you could write the whole thing but... why?
 dt = read.csv(paste0(dir, 'datim_sample_data.csv'))
 
 # convert the data to a 'data table'
-# data tables are a special format that makes data cleaning easier
+# data tables are a special file format that makes data cleaning easier
 dt = data.table(dt)
 
 # --------------------------------------------
@@ -54,6 +58,7 @@ dt = data.table(dt)
 View(dt)
 
 # view the top 5 lines of the data 
+# HTS_TST refers to HIV tests, and HTS_TST_POS is the number who tested HIV+
 head(dt, n = 5) # change n = to see a different number of lines
 
 # how many rows are there in the data set?
@@ -68,6 +73,7 @@ str(dt)
 
 # hmmm... I only see characters and integers (numerics) 
 # let's check the type of a specific variable
+# what type of variable is 'quarter' (refers to the fiscal quarter)
 class(dt$quarter)
 
 # --------------------
@@ -77,13 +83,13 @@ class(dt$quarter)
 # this one is hard - how many specific sites are in the data set?
 length(unique(dt$region))
 
-# print me a list of every unique site!!!!
+# print me a list of every unique region!!!!
 unique(dt$region)
 
 # now it's your turn:
 # add code that prints the unique values of the variable
 
-dt$quarter = factor(dt$quarter)
+
 
 # --------------------
 
@@ -98,7 +104,18 @@ dt$quarter = factor(dt$quarter)
 dt$quarter = factor(dt$quarter) # set it so Q1 is before Q2 in a graph
 # --------------------
 
+# CALCULATE STUFF
 # --------------------
+# step by step - specify rows, columns, and any "sorts"
+dt[variable=='HTS_TST'] # select the rows that show HIV tests
+dt[variable=='HTS_TST', sum(value)] # for the rows with hiv tests, sum up the values
+dt[variable=='HTS_TST', sum(value), by = sex ] # for the rows with hiv tests, sum up the values by sex
+
+# literally you just did everything you need to do in R ever
+
+# --------------------
+# ok let's calculate some stuff 
+
 # what was the total number of hiv tests performed in these data?
 dt[variable=='HTS_TST', sum(value)]
 
@@ -111,12 +128,11 @@ dt[variable=='HTS_POS', sum(value), by = quarter]
 # what was the total number of positive tests for women in these data by quarter?
 dt[variable=='HTS_POS' & sex=='Female', sum(value), by = quarter]
 
-# now you try - how many men were tested for HIV in Q1?
-# write code here:
+# now you try - what was the total number of hiv tests performed on men?
 
+# you should get 28,513 men - if you don't you did wrong
 
-
-# you should get 28,513 men
+# what was the total number of hiv tests performed on men in Q2?
 
 # --------------------
 
@@ -138,6 +154,8 @@ ggplot(plot_dt[variable=='HTS_TST'],
   geom_line()+ # adds a line graph
   theme_bw() # makes the graph pretty
 
+# NICE! An upward trend
+
 # --------------------
 # make an ugly 'spaghetti plot' at the regional level
 
@@ -152,11 +170,14 @@ ggplot(dt[variable=='HTS_TST'],
 # --------------------
 # BONUS: make a stacked bar graph - this is advanced R, but fun as a bonus:
 
-ggplot(plot_dt, aes(x = quarter, y = value, fill = sex))+
+ggplot(plot_dt, aes(x = quarter, y = value, fill = factor(sex)))+
   geom_bar(position = "dodge", stat = "identity")+
   facet_wrap(~variable, scales = 'free_y')+
+  geom_text(aes(label = value), vjust = -0.2, position = position_dodge(0.9))+
+  scale_fill_manual(values = c('#f03b20', '#feb24c'))+
   theme_bw()+
-  labs(x = 'Fiscal Quarter', y = 'Clients', fill = 'Sex')
+  labs(x = 'Fiscal Quarter', y = 'Clients', fill = 'Sex', 
+       title = 'HIV tests and clients who tested HIV+ by sex, FY21')
 
 # --------------------------------------------
 
