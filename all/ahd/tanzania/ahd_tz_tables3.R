64bd57a994a6c1014344d5a5a545f9219d6d5cb1
Caitlin Carelli <caitlinobc@gmail.com>
2021-12-22 13:39:14 -0800

    Update ahd_tz_tables3.R

--- all/ahd/tanzania/ahd_tz_tables3.R
+++ all/ahd/tanzania/ahd_tz_tables3.R
@@ -31,8 +31,8 @@ setwd(dir)
 # set the output directory for prepped data 
 prepDir = paste0(dir, 'prepped/')
 
-# set the output directory for visuals
-outDir = paste0(dir, 'outputs/')
+# set the output directory for tables
+outDir = paste0(dir, 'outputs/tables/')
 
 # ------------------------
 # import the data
@@ -41,12 +41,12 @@ dt = readRDS(paste0(prepDir, 'full_data.RDS'))
 # ------------------------
 
 # ----------------------------------------------
-# DEMOGRAPHIC TABLES 
+# DEMOGRAPHIC & ELIGIBILITY TABLES 
 # ----------------------------------------------
 # create some demographic tables to show study eligibility
 
 # ------------------------
-# participants by sex, age category
+# participants by sex, age category, period
 dt_sa = dt[ ,.(value = length(unique(pid))), by =.(sex, age_cat, period)]
 dt_sa = dcast(dt_sa, age_cat~period+sex)
 dt_sa[ , b_Total:=(b_Female+b_Male)]
@@ -58,7 +58,7 @@ write.csv(dt_sa, paste0(outDir, 'participants_age_sex.csv'))
 # ------------------------
 
 # ------------------------
-# participants by site, sex
+# participants by site, sex, period
 dt_ss = dt[ ,.(value = length(unique(pid))), by =.(sex, dhisname, period)]
 dt_ss = dcast(dt_ss, dhisname~period+sex)
 dt_ss[ , b_Total:=(b_Female+b_Male)]
@@ -70,7 +70,7 @@ write.csv(dt_ss, paste0(outDir, 'participants_site_sex.csv'))
 # ------------------------
 
 # ------------------------
-# age distribution by site
+# age distribution by site, period
 dt_as = dt[ ,.(value = length(unique(pid))), by =.(age_cat, site, period)]
 dt_as = dcast(dt_as, age_cat~period+site)
 
@@ -79,7 +79,7 @@ write.csv(dt_as, paste0(outDir, 'participants_site_age.csv'))
 # ------------------------
 
 # ------------------------
-# study eligibility by sex
+# study eligibility by sex, period
 dt_elig = dt[, .(value = length(unique(pid))) , by = .(ahd_elig, sex, period)]
 dt_elig2 = dt[, .(value = length(unique(pid))) , by = .(ahd_elig, period)]
 dt_elig2[ , sex:='Total']
@@ -91,21 +91,54 @@ write.csv(dt_elig, paste0(outDir, 'eligibilty_sex.csv'))
 # ------------------------
 
 # ----------------------------------------------
-# OUTCOME TABLES: TB SCREENING
+# OUTCOME TABLES: HIV TESTING/STATUS
 # ----------------------------------------------
 
+# ----------------------------------------------
+# OUTCOME TABLES: TB SCREENING
+# ----------------------------------------------
+# no missing data in tb symptom screening
 
+# ------------------------
+# screened and not screened for tb by period
+tb_scrn = dt[, .(scrn = sum(tbsympscrn), type = 'Screened'), by = period]
+tb_scrn2 = dt[tbsympscrn==0, .(scrn = length(unique(pid)), type = 'Not screened'), by = period]
+tb_scrn = rbind(tb_scrn, tb_scrn2)
+tb_scrn = dcast(tb_scrn, type~period, value.var = 'scrn')
+setnames(tb_scrn, c('Screened for TB', 'Pre-intervention', 'Post-intervention'))
 
+# export the table
+write.csv(tb_scrn, paste0(outDir, 'screened_for_tb.csv'))
+# ------------------------
 
+# ------------------------
+# screened for tb by sex, age, period
+tb_scrn_as = dt[, .(scrn = sum(tbsympscrn)), by = .(sex, age_cat, period)]
+tb_scrn_as = dcast(tb_scrn_as, age_cat~period+sex, value.var = 'scrn')
 
+# export the table
+write.csv(tb_scrn_as, paste0(outDir, 'screened_for_tb_sex_age.csv'))
 
+# ------------------------
 
+# ------------------------
+# screened and not screened for tb by sex, age, period
+setnames(tb_scrn_as,c('age_cat', 'b_Female_s', 'b_Male_s', 
+           'e_Female_s', 'e_Male_s'))
 
+# table of not screened for tb
+tb_ns_as = dt[tbsympscrn==0, .(scrn = length(unique(pid))), by = .(sex, age_cat, period)]
+tb_ns_as = dcast(tb_ns_as, age_cat~period+sex, value.var = 'scrn')
+tb_all = merge(tb_scrn_as, tb_ns_as, by = 'age_cat', all = T)
 
+# rearrange variables in order of table - by sex, then screening status
+tb_all = tb_all[ ,.(age_cat, b_Female_s, b_Female, b_Male_s, b_Male,
+            e_Female_s, e_Female, e_Male_s, e_Male)]
 
+# export the table
+write.csv(tb_all, paste0(outDir, 'screened_ns_for_tb_sex_age.csv'))
 
-
-
+# ------------------------
 
 
 
