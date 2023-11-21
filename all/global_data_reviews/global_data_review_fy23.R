7536d076fa3ce8e6e3d942284de1d6197a02b461
Caitlin O'Brien-Carelli <ccarelli@pedaids.org>
2023-11-21 14:55:40 -0800

    Update global_data_review_fy23.R

--- all/global_data_reviews/global_data_review_fy23.R
+++ all/global_data_reviews/global_data_review_fy23.R
@@ -1,9 +1,10 @@
 # ----------------------------------------------
 # Caitlin O'Brien-Carelli
 #
-# 6/2/2023
-# GDR presentation - June FY23
-# imports, cleans, and analyzes data for the GDR
+# 11/21/2023
+# GDR presentation - December 2023
+# Imports, cleans, and analyzes data for the GDR
+# Created vector maps to be presented at the GDR
 # ----------------------------------------------
 
 # ------------------------
@@ -42,7 +43,7 @@ plot(map)
 coord = data.table(fortify(map))
 
 # merge in the names of the countries to match the data 
-names = data.table(cbind(country = c(map@data$COUNTRY), id =  c(0, seq(1:47))))
+names = data.table(cbind(country = c(map@data$COUNTRY), id =  seq(1:48)))
 coord = merge(coord, names, by = 'id')
 
 # fix the map by creating a South Africa specific map
@@ -55,25 +56,27 @@ s_africa_layer = geom_polygon(aes(x = long, y = lat, group = group),
 # IMPORT & FORMAT DATA 
 # ----------------------------------------------
 # import prep_new and rename the columns
-dt1 = data.table(read.csv(paste0(dir, 'gdr_23_maps_data.csv')))
+dt1 = data.table(read.csv(paste0(dir, 'gdr_dec23_maps_data.csv')))
 
-# split the single column
-setnames(dt1, 'col')
-
-# unmess up the data 
-dt1[ , award:=unlist(lapply(str_split(col, '\\\t'), '[', 1))]
-dt1[ , country:=unlist(lapply(str_split(col, '\\\t'), '[', 2))]
-dt1[ , fq:=unlist(lapply(str_split(col, '\\\t'), '[', 3))]
-dt1[ , age:=unlist(lapply(str_split(col, '\\\t'), '[', 4))]
-dt1[ , hts_tst:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 5)))]
-
-dt1[ , hts_tst_pos:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 6)))]
-dt1[ , tx_curr:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 7)))]
-dt1[ , col:=NULL]
+# use this code if the .csv converts to a single line data set
+# # split the single column
+# setnames(dt1, 'col')
+# 
+# # unmess up the data 
+# dt1[ , award:=unlist(lapply(str_split(col, '\\\t'), '[', 1))]
+# dt1[ , country:=unlist(lapply(str_split(col, '\\\t'), '[', 2))]
+# dt1[ , fq:=unlist(lapply(str_split(col, '\\\t'), '[', 3))]
+# dt1[ , age:=unlist(lapply(str_split(col, '\\\t'), '[', 4))]
+# dt1[ , hts_tst:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 5)))]
+# 
+# dt1[ , hts_tst_pos:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 6)))]
+# dt1[ , tx_curr:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 7)))]
+# dt1[ , col:=NULL]
 
 setnames(dt1, c('award', 'country', 'fq', 'age', 'hts_tst', 'hts_tst_pos', 'tx_curr'))
 
 # create a data set at the country, not award, level
+# ihap values for q2 fy23 added manually (sent via e-mail, not in datim)
 dt = dt1[ ,.(hts_tst = sum(hts_tst, na.rm=T), hts_tst_pos = sum(hts_tst_pos, na.rm=T),
              tx_curr = sum(tx_curr, na.rm=T)), by = .(country, fq, age)]
 
@@ -88,49 +91,51 @@ dt_all = dt1[ ,.(hts_tst = sum(hts_tst, na.rm=T), hts_tst_pos = sum(hts_tst_pos,
 dt_all[ , hts_rate:=round(((hts_tst_pos/hts_tst)*100), 1)]
 
 # create a data set at the country level without age disaggregations for all of fy23
-dt_all23 = dt1[ ,.(hts_tst = sum(hts_tst, na.rm=T),
+# do not include tx_curr as it is not additive
+dt_all23 = dt1[grepl('23', fq),.(hts_tst = sum(hts_tst, na.rm=T),
                    hts_tst_pos = sum(hts_tst_pos, na.rm=T)), by = .(country)]
 
 # tx_curr is not additive for the fy23 total data
-tx = dt_all[fq=='FY23 Q2', .(tx_curr)]
+# use the most recent total value - e.g. Q4
+tx = dt_all[fq=='FY23 Q4', .(tx_curr)]
 dt_all23 = cbind(dt_all23, tx)
 
 # calculate test positivity at the country level
 dt_all23[ , hts_rate:=round(((hts_tst_pos/hts_tst)*100), 1)]
-
-#----------------------
-# ICT map
-
-# import prep_new and rename the columns
-ind = data.table(read.csv(paste0(dir, 'hts_index_contrib.csv')))
-setnames (ind, 'col')
-
-ind[ , award:=unlist(lapply(str_split(col, '\\\t'), '[', 1))]
-ind[ , country:=unlist(lapply(str_split(col, '\\\t'), '[', 2))]
-ind[ , pos:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 3)))]
-ind[ , index_pos:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 4)))]
-ind[ , col:=NULL]
-
-setnames(ind, c('award', 'country', 'pos', 'index_pos'))
-
-# add in the ihap and lisanga data
-
-vector1 = c(award = 'IHAP', country = 'DRC', pos = 878, index_pos = 420)
-vector2 = c(award = 'Lisanga', country = 'DRC', pos = 1878, index_pos = 675)
-vector = data.table(rbind(vector1, vector2))
-ind = rbind(ind, vector)
-ind[ , pos:=as.numeric(pos)]
-ind[ , index_pos:=as.numeric(index_pos)]
-
-# create a variable showing the contribution of index case testing
-ind = ind[ , .(pos = sum(pos), index_pos = sum(index_pos)), by = country]
-ind[ , contribution:=round(100*(index_pos/(pos+index_pos)), 1)]
-ind = ind[ ,.(country, contribution)]
-
-# merge the contributions into the main data set
-dt_all23 = merge(dt_all23, ind, by = 'country')
-
-#----------------------
+# 
+# #----------------------
+# # ICT map
+# 
+# # import prep_new and rename the columns
+# ind = data.table(read.csv(paste0(dir, 'hts_index_contrib.csv')))
+# setnames (ind, 'col')
+# 
+# ind[ , award:=unlist(lapply(str_split(col, '\\\t'), '[', 1))]
+# ind[ , country:=unlist(lapply(str_split(col, '\\\t'), '[', 2))]
+# ind[ , pos:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 3)))]
+# ind[ , index_pos:=as.numeric(unlist(lapply(str_split(col, '\\\t'), '[', 4)))]
+# ind[ , col:=NULL]
+# 
+# setnames(ind, c('award', 'country', 'pos', 'index_pos'))
+# 
+# # add in the ihap and lisanga data
+# 
+# vector1 = c(award = 'IHAP', country = 'DRC', pos = 878, index_pos = 420)
+# vector2 = c(award = 'Lisanga', country = 'DRC', pos = 1878, index_pos = 675)
+# vector = data.table(rbind(vector1, vector2))
+# ind = rbind(ind, vector)
+# ind[ , pos:=as.numeric(pos)]
+# ind[ , index_pos:=as.numeric(index_pos)]
+# 
+# # create a variable showing the contribution of index case testing
+# ind = ind[ , .(pos = sum(pos), index_pos = sum(index_pos)), by = country]
+# ind[ , contribution:=round(100*(index_pos/(pos+index_pos)), 1)]
+# ind = ind[ ,.(country, contribution)]
+# 
+# # merge the contributions into the main data set
+# dt_all23 = merge(dt_all23, ind, by = 'country')
+# 
+# #----------------------
 
 # ----------------------------------------------
 # MAPS
@@ -156,7 +161,7 @@ labels[!is.na(hts_tst) , hts_tst_label:=paste0(country, ": ", hts_tst)]
 labels[!is.na(hts_tst) , hts_tst_pos_label:=paste0(country, ": ", hts_tst_pos)]
 labels[!is.na(hts_tst) , tx_curr_label:=paste0(country, ": ", tx_curr)]
 labels[!is.na(hts_tst) , hts_rate_label:=paste0(country, ": ", hts_rate, '%')]
-labels[!is.na(contribution) , contribution_label:=paste0(country, ": ", contribution, '%')]
+# labels[!is.na(contribution) , contribution_label:=paste0(country, ": ", contribution, '%')]
 
 # --------------------
 # map of hts_tst, fy23 
@@ -207,7 +212,7 @@ ggplot(fy23_map[country!='South Africa'], aes(x=long, y=lat, group=group, fill=h
 # ------------------------
 
 # --------------------
-# map of art cohort, q2 fy23 
+# map of art cohort, q4 fy23 
 
 ggplot(fy23_map[country!='South Africa'], aes(x=long, y=lat, group=group, fill=tx_curr)) + 
   coord_fixed() +
@@ -215,7 +220,7 @@ ggplot(fy23_map[country!='South Africa'], aes(x=long, y=lat, group=group, fill=t
   geom_path(size=0.01) + 
   scale_fill_gradientn(colors = brewer.pal(9, 'Greens'), na.value='#ffffff',labels=comma) + 
   theme_void(base_size =16) +
-  labs(fill="ART Cohort (TX_CURR), Q2 FY23")+
+  labs(fill="ART Cohort (TX_CURR), Q4 FY23")+
   theme( text=element_text(size=18))+
   s_africa_layer+
   geom_label_repel(data = labels, aes(label = tx_curr_label, 
@@ -223,19 +228,19 @@ ggplot(fy23_map[country!='South Africa'], aes(x=long, y=lat, group=group, fill=t
 # ------------------------
 
 # --------------------
-# map of ict contribution, q2 fy23 
-
-ggplot(fy23_map[country!='South Africa'], aes(x=long, y=lat, group=group, fill=contribution)) + 
-  coord_fixed() +
-  geom_polygon() + 
-  geom_path(size=0.01) + 
-  scale_fill_gradientn(colors = brewer.pal(9, 'Purples'), na.value='#ffffff',labels=comma) + 
-  theme_void(base_size =16) +
-  labs(fill="ICT Contribution (%), Q2 FY23")+
-  theme( text=element_text(size=18))+
-  s_africa_layer+
-  geom_label_repel(data = labels, aes(label = contribution_label, 
-                x=long, y=lat, group=country), inherit.aes=FALSE, size=5)
+# map of ict contribution, fy23 
+# 
+# ggplot(fy23_map[country!='South Africa'], aes(x=long, y=lat, group=group, fill=contribution)) + 
+#   coord_fixed() +
+#   geom_polygon() + 
+#   geom_path(size=0.01) + 
+#   scale_fill_gradientn(colors = brewer.pal(9, 'Purples'), na.value='#ffffff',labels=comma) + 
+#   theme_void(base_size =16) +
+#   labs(fill="ICT Contribution (%), FY23")+
+#   theme( text=element_text(size=18))+
+#   s_africa_layer+
+#   geom_label_repel(data = labels, aes(label = contribution_label, 
+#                 x=long, y=lat, group=country), inherit.aes=FALSE, size=5)
 # ------------------------
 
 # -------------------------------------------
